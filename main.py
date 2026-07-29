3902095fafe111fc2b5b8672a14b9a4153e0423f
25jeimer-lgtm <25jeimer@gmail.com>
2026-07-29 11:12:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import telebot
+from flask import Flask, request
+import os
+
+TOKEN = "ВАШ_ТОКЕН_СЮДА"
+bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Я Cortex AI. Скоро здесь будут все функции!")
+
+@app.route('/' + TOKEN, methods=['POST'])
+def webhook():
+    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+    bot.process_new_updates([update])
+    return 'ok', 200
+
+@app.route('/')
+def index():
+    return 'Cortex AI Bot is running!'
+
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url='https://ваш-логин.onrender.com/' + TOKEN)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
